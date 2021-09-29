10813ba5586cacf4986247c31831eded5a193456
charasullia <charanrajr713@gmail.com>
2021-09-29 20:23:19 +0530

    [charansullia]added:Simple LinkedList

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+        internal Node head;
+        internal void add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head==null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next!=null)
+                {
+                   temp= temp.next;
+                }
+                   temp.next = node;
+            }
+            Console.WriteLine("{0} inserted to LinkedList",node.data);
+        }
+        internal void display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while(temp!=null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.add(56);
+            list.add(30);
+            list.add(70);
+            list.display();
+        }
+    }
+}
