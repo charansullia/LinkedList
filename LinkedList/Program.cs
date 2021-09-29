143643314cffdf8ecba465db5a54fe7be20498ee
charasullia <charanrajr713@gmail.com>
2021-09-29 20:31:02 +0530

    [charansullia]added:Adding Data To LinkedList

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,8 +7,8 @@ namespace LinkedList
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            list.add(56);
             list.add(30);
+            list.add(56);
             list.add(70);
             list.display();
         }
