79b3513fa5c8238ff8385067fc9866baa01dbb50
charasullia <charanrajr713@gmail.com>
2021-10-03 13:53:48 +0530

    [charansullia]added:Data Inserted to Particular Position

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -25,7 +25,7 @@ namespace LinkedList
             }
             Console.WriteLine("{0} inserted to LinkedList",node.data);
         }
-        internal void display()
+        internal void Display()
         {
             Node temp = this.head;
             if(temp==null)
@@ -38,6 +38,38 @@ namespace LinkedList
                 Console.WriteLine(temp.data+" ");
                 temp = temp.next;
             }
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0) 
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;
+                }
+                Console.WriteLine("{0} inserted to particular Position in LinkedList");
+                if (position != 1)
+                {
+                    Console.WriteLine("Position out of range");
+                }
+            }
+            return head;
         }
     }
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,10 +7,11 @@ namespace LinkedList
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            list.add(30);
+         //   list.add(30);
             list.add(56);
             list.add(70);
-            list.display();
+            list.InsertAtParticularPosition(2,30);
+            list.Display();
         }
     }
 }
